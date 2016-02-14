d1b6cfcfc22d1e69f8af173566b36f8b9b5f2f81
Alexander Fahlke <alexander@fahlke.org>
2016-02-14 16:34:30 +0100

    first try of adding the travis-ci stuff

---
+++ setup.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+try:
+    # Use setuptools if available, for install_requires (among other things).
+    import setuptools
+    from setuptools import setup
+except ImportError:
+    setuptools = None
+    from distutils.core import setup
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+kwargs = {}
+version = "0.1.0"
+
+if setuptools is not None:
+    # If setuptools is not available, you're on your own for dependencies.
+    install_requires = []
+    tests_require = []
+    if sys.version_info < (2, 7):
+        install_requires.append('pytz')
+        tests_require.append("unittest2")
+    if sys.version_info < (3, 5):
+        install_requires.append('pytz')
+        tests_require.append("unittest2")
+    kwargs['install_requires'] = install_requires
+    kwargs['tests_require'] = install_requires
+
+if sys.argv[-1] == 'publish':
+    os.system("git tag -a %s -m 'v%s'" % (version, version))
+    print("Published version %s, do `git push --tags` to push new tag to remote" % version)
+    sys.exit()
+
+setup(
+    name="hellotravis-python",
+    version=version,
+    description="A repository for testing stuff with python, travis-ci and coveralls",
+    long_description="\n\n".join([
+        open(os.path.join(base_dir, "README.md"), "r").read(),
+        open(os.path.join(base_dir, "CHANGELOG.md"), "r").read()
+    ]),
+    url="http://github.com/alexanderfahlke/hellotravis-python",
+    author="Alexander Fahlke",
+    author_email="alexander.fahlke@gmail.com",
+    maintainer="Alexander Fahlke",
+    maintainer_email="alexander.fahlke@gmail.com",
+    packages=["hellotravis-python"],
+    zip_safe=False,
+    test_suite="tests.get_tests",
+    **kwargs
+)
